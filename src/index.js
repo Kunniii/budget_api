1df6c14cec7f466dc475799656f9ed407d368fb6
Kunniii <truongnp@codechovui.dev>
2024-03-30 10:57:48 +0700

    scaffold: final compose for dev, init prisma orm

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-// simple express server
 import express from "express";
+import bodyParser from "body-parser";
+const clog = console.log;
 var app = express();
 var port = process.env.PORT || 3000;
-
-app.get("/", function (req, res) {
-  res.json({
-    OK: true,
-  });
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.listen(port, (err) => {
   if (err) {
